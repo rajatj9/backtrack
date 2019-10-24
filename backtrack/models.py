f319cc44ca9524969ab36e497e1e7ca5d71ef806
Rajat Jain <jain.rajat50@gmail.com>
2019-10-25 00:15:01 +0800

    Revert "add estimate attribute and default value for status"
    This reverts commit a3a00b18

--- backtrack/models.py
+++ backtrack/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 # Create your models here.
 class PBI(models.Model):
-    name = models.CharField(max_length=250,default="")
-    description = models.TextField(default="")
-    priority = models.IntegerField(default=0)
-    estimate = models.IntegerField(default=0)
-    status = models.CharField(max_length=50,default="Not Yet Started")
-
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    priority = models.IntegerField()
+    status = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
--- backtrack/urls.py
+++ backtrack/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .api import views
 
 urlpatterns = [
-    path('pbi/', views.PBIListView.as_view(), name=None),
-    path('pbi/create/', views.PBICreateView.as_view(), name=None),
-    path('pbi/<int:pk>/', views.PBIDetailView.as_view(), name=None)
+    path('', views.PBIListView.as_view(), name=None),
+    path('create/', views.PBICreateView.as_view(), name=None),
+    path('<int:pk>/', views.PBIDetailView.as_view(), name=None)
 ]
