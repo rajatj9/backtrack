c1fb62bc8b30ca68341b8591814037dcb8517470
MarcoBrian <mb.widjaja@yahoo.com>
2019-11-28 00:26:21 +0700

    set up single type user

--- api/settings.py
+++ api/settings.py
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
     'backtrack',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
     'corsheaders'
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -125,3 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#user login settings
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
+ACCOUNT_EMAIL_REQUIRED = False
--- backtrack/admin.py
+++ backtrack/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import PBI
+from .models import *
 
 # Register your models here.
 admin.site.register(PBI)
+admin.site.register(Tasks)
+admin.site.register(Manager)
+admin.site.register(Developer)
+admin.site.register(Sprint)
+admin.site.register(Project)
